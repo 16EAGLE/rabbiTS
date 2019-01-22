eaaf789fae3ea5ea75396bf9e535c7e8cdd4269e
16EAGLE <jxsw@web.de>
2019-01-22 09:43:43 +0100

    udpated internal

--- R/internal.R
+++ R/internal.R
@@ -8,39 +8,7 @@
 #' @keywords internal
 #' @noRd
 
-out <- function(input, type = 1, ll = NULL, msg = FALSE, sign = "", verbose = getOption("temp.verbose")){
-  if(is.null(ll)) if(isTRUE(verbose)) ll <- 1 else ll <- 2
-  if(type == 2 & ll <= 2){warning(paste0(sign,input), call. = FALSE, immediate. = TRUE)}
-  else{if(type == 3){stop(input,call. = FALSE)}else{if(ll == 1){
-    if(msg == FALSE){ cat(paste0(sign,input),sep="\n")
-    } else{message(paste0(sign,input))}}}}
-}
-
-#' Simplifies check of variables being FALSE
-#'
-#' @param evaluate variable or expression to be evaluated
-#'
-#' @keywords internal
-#' @noRd
-is.FALSE <- function(evaluate){if(evaluate == FALSE){return(TRUE)}else{return(FALSE)}}
-
-
-#' Simplifies check of variables being TRUE
-#'
-#' @param evaluate variable or expression to be evaluated
-#'
-#' @keywords internal
-#' @noRd
-is.TRUE <- function(evaluate){if(evaluate == TRUE){return(TRUE)}else{return(FALSE)}}
-
-
-#' Checks, if specific command is available
-#'
-#' @param cmd command
-#' @importFrom devtools system_check
-#' @keywords internal
-#' @noRd
-check.cmd <- function(cmd){
-  sc <- try(system_check(cmd, quiet = TRUE),silent = TRUE)
-  if(class(sc) == "try-error"){return(FALSE)}else{return(TRUE)}
+.fun <- function(x){
+  #some function
+  return(x)
 }
