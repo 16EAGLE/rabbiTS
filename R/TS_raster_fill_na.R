34f120f1b5e84ea9d05b38ddda6537d651f4dabe
Frederic Schwarzenbacher <frederic@ubuntu_server.mshome.net>
2019-01-24 16:21:30 +0000

    Improved layout and comments

--- R/TS_raster_fill_na.R
+++ R/TS_raster_fill_na.R
@@ -29,7 +29,7 @@ TS_raster_fill_na <- function(stack_obj){
     # Check if there are NA values to replace
     if (any(which(is.na(temp_matrix)))) {
     # Start processing  
-      plan(multiprocess) #Set up for Multi core processing 
+      plan(multiprocess) #Setup for Multi core processing 
     # Start of interpolation to temporary matrix  
       result <- future_apply(temp_matrix, 2, FUN = seq_fill_na)
     # Writing results into original Stack or Brick Structure  
@@ -41,10 +41,10 @@ TS_raster_fill_na <- function(stack_obj){
       print("No NA values in TimeSeries Object. Returned original input")
       return(stack_obj)
     }
-    # when finished, return the result
+    # When finished, return the result
     return(stack_obj)
   } else {
-    # return input if is not stack or brick object
+    # Return input if is not stack or brick object
     print("Input is not a RasterStack or RasterBrick")
     return(stack_obj)
   }
--- R/seq_fill_na.R
+++ R/seq_fill_na.R
@@ -1,47 +1,28 @@
 #' NA vector fill function
-
 #'
-
 #' \code{seq_fill_na} fills NA values within a vector with linearly interpolated numeric values.
-
-#' 
-
 #'
-
 #' @param vector numeric vector
-
 #'
-
 #' @return numeric vector without NA values.
-
 #'
-
 #' @importFrom zoo na.fill
-
 #' 
-
 #' @export
 
-
-
 seq_fill_na <- function(vector){
-  
-  
-  
-  # here you start coding, for example, start with sum checks:
-  
-  
-  
-  if(!inherits(vector, "numeric")) cat("Argument vector should be a 'numeric' object.")
-  
-  if (length(which(!is.na(vector)))<2) { # interpolation needs at least 2 numeric values
+  # Check if input is numeric
+  if(!inherits(vector, "numeric")){
+    print("Invalid Input. Vector has to be a 'numeric' object.")
+    return(vector)
+  }
+  # Interpolation needs at least 2 numeric values
+  if (length(which(!is.na(vector)))<2) {
     return(vector)
     } else {
+  # Staring interpolation    
     vector <- na.fill(vector,"extend")  # fill NA values
   }
-  
-  # when finished, return the result
-  
+  # When finished, return the result
   return(vector)
-  
 }
